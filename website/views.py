4cc08c36022242166ad3f31a36e4051c417066c1
IanTth <ianrendrick@gmail.com>
2022-06-19 13:58:57 -0300

    base do Flask

---
+++ website/views.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("index.html")
+
+# rotas #
+# Esse arquivo define as rotas do Python, acredito que não teremos mais que um arquivo html, mas se for necessário, eu crio mais rotas #
